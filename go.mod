c581169978aafa9f26584a61a8dd22440edd879a
Muhammad Afuzarahman <theafuza@gmail.com>
2024-06-09 13:41:25 -0700

    Implement OpenAPI to test routes

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.22.4
 
 require (
 	github.com/go-chi/chi v1.5.5
-	github.com/gorilla/schema v1.3.0
 	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- router/router.go
+++ router/router.go
@@ -1,9 +1,12 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/afutofu/go-api-starter/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	log "github.com/sirupsen/logrus"
 )
 
 func SetupRouter() *chi.Mux {
@@ -16,6 +19,22 @@ func SetupRouter() *chi.Mux {
 	router.Get("/todos/{id}", handlers.GetTodo)
 	router.Put("/todos/{id}", handlers.UpdateTodo)
 	router.Delete("/todos/{id}", handlers.DeleteTodo)
+
+	// Serve OpenAPI YAML file
+	router.Get("/docs/openapi.yaml", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./docs/openapi.yaml")
+	})
+
+	// Serve Swagger UI files
+	router.Get("/swagger/*", func(w http.ResponseWriter, r *http.Request) {
+		http.StripPrefix("/swagger/", http.FileServer(http.Dir("./swagger-ui-dist"))).ServeHTTP(w, r)
+	})
+
+	// Serve modified index.html
+	router.Get("/swagger", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./swagger-ui-dist/index.html")
+	})
+	log.Info("Router setup complete")
 
 	return router
 }
