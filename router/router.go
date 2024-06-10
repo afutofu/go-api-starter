9bec67390125ce78bbcd50482069b3b0cc852ac1
Muhammad Afuzarahman <theafuza@gmail.com>
2024-06-09 22:04:48 -0700

    Create OpenAPI UI for auth API

--- router/router.go
+++ router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/afutofu/go-api-starter/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -15,6 +17,21 @@ func SetupRouter() *chi.Mux {
 	router.Post("/register", handlers.Register)
 	router.Post("/login", handlers.Login)
 	router.Post("/logout", handlers.Logout)
+
+	// Serve OpenAPI YAML file
+	router.Get("/docs/openapi.yaml", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./docs/openapi.yaml")
+	})
+
+	// Serve Swagger UI files
+	router.Get("/swagger/*", func(w http.ResponseWriter, r *http.Request) {
+		http.StripPrefix("/swagger/", http.FileServer(http.Dir("./swagger-ui-dist"))).ServeHTTP(w, r)
+	})
+
+	// Serve modified index.html
+	router.Get("/swagger", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./swagger-ui-dist/index.html")
+	})
 
 	log.Info("Router setup complete")
 
