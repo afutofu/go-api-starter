b8af2d7e5f6699fd5ea1739f6eff0e7d729371ca
Muhammad Afuzarahman <theafuza@gmail.com>
2024-06-09 18:37:58 -0700

    Remove OpenAPI integration for this branch

--- router/router.go
+++ router/router.go
@@ -1,8 +1,6 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/afutofu/go-api-starter/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -20,20 +18,6 @@ func SetupRouter() *chi.Mux {
 	router.Put("/todos/{id}", handlers.UpdateTodo)
 	router.Delete("/todos/{id}", handlers.DeleteTodo)
 
-	// Serve OpenAPI YAML file
-	router.Get("/docs/openapi.yaml", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./docs/openapi.yaml")
-	})
-
-	// Serve Swagger UI files
-	router.Get("/swagger/*", func(w http.ResponseWriter, r *http.Request) {
-		http.StripPrefix("/swagger/", http.FileServer(http.Dir("./swagger-ui-dist"))).ServeHTTP(w, r)
-	})
-
-	// Serve modified index.html
-	router.Get("/swagger", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./swagger-ui-dist/index.html")
-	})
 	log.Info("Router setup complete")
 
 	return router
