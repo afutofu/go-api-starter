0cdba6a9185186091299d778f4f21f6cb6a7cbbd
Muhammad Afuzarahman <theafuza@gmail.com>
2024-06-25 18:01:48 -0700

    Implement CORS

--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/afutofu/go-api-starter/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -11,6 +12,17 @@ func SetupRouter() *chi.Mux {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
+
+	// CORS middleware setup
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // Replace with your frontend URL
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	router.Use(cors.Handler)
 
 	router.Post("/todos", handlers.CreateTodo)
 	router.Get("/todos", handlers.GetTodos)
